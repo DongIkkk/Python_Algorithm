85e8ac0e044a68b3a1f3dd56b3a49dc8fea89be0
DongIkkk <w8h0412@gmail.com>
2022-11-09 19:54:21 +0900

    week_1 6

--- week_1/6_gym_suit.py
+++ week_1/6_gym_suit.py
@@ -1,44 +1,28 @@
 def solution(n, lost, reserve):
-    for i in lost:
-        if i in reserve:
-            lost.remove(i)
-            reserve.remove(i)
+    # 이렇게 지우면 안됨 index 밀린다
+    # for i in lost:
+    #     if i in reserve:
+    #         lost.remove(i)
+    #         reserve.remove(i)
+    new_lost = [l for l in lost if l not in reserve]
+    new_reserve = [r for r in reserve if r not in lost]
 
-    lost2 = lost.copy()
-    reserve2 = reserve.copy()
+    lost2 = new_lost.copy()
+    reserve2 = new_reserve.copy()
 
-    if n in reserve:
-        for i in reserve:
-            if (int(i) - 1) in lost:
-                lost.remove(int(i) - 1)
-            elif (int(i) + 1) in lost:
-                lost.remove(int(i) + 1)
-        answer = n - len(lost)
+    for i in new_reserve:
+        if (i - 1) in new_lost:
+            new_lost.remove(i - 1)
+        elif (i + 1) in new_lost:
+            new_lost.remove(i + 1)
+    result = n - len(new_lost)
+    for i in reserve2:
+        if (i + 1) in lost2:
+            lost2.remove(i + 1)
+        elif (i - 1) in lost2:
+            lost2.remove(i - 1)
+    result2 = n - len(lost2)
 
-    elif 1 in reserve:
-        for i in reserve:
-            if (int(i) + 1) in lost:
-                lost.remove(int(i) + 1)
-            elif (int(i) - 1) in lost:
-                lost.remove(int(i) - 1)
-        answer = n - len(lost)
-
-    else:
-        for i in reserve:
-            if (int(i) - 1) in lost:
-                lost.remove(int(i) - 1)
-            elif (int(i) + 1) in lost:
-                lost.remove(int(i) + 1)
-        result = n - len(lost)
-        for i in reserve2:
-            if (int(i) + 1) in lost2:
-                lost2.remove(int(i) + 1)
-            elif (int(i) - 1) in lost:
-                lost2.remove(int(i) - 1)
-        result2 = n - len(lost)
-
-        answer = max(result, result2)
-
-    answer = n - len(lost)
+    answer = max(result, result2)
 
     return answer
