03b8ff23a41b2c1094590ae97eb2fdedb29be581
DongIkkk <w8h0412@gmail.com>
2022-11-09 22:07:02 +0900

    week_1 10 dictionary

--- week_1/10_marathon_fail.py
+++ week_1/10_marathon_fail.py
@@ -6,9 +6,31 @@
 #     return answer
 
 # 2.시간초과
+# def solution(participant, completion):
+#     for i in participant:
+#         if i in completion:
+#             completion.remove(i)
+#         else:
+#             return i
+
+# 3. 해시 - 딕셔너리로해보기
 def solution(participant, completion):
+    hash = {}
     for i in participant:
-        if i in completion:
-            completion.remove(i)
+        if i in hash:
+            hash[i] += 1
         else:
-            return i
+            hash[i] = 1
+
+    for j in completion:
+        hash[j] -= 1
+
+    # value값으로 key 찾기
+    for k, v in hash.items():
+        if v == 1:
+            return k
+
+
+p = ["mislav", "stanko", "mislav", "ana"]
+c = ["stanko", "ana", "mislav"]
+print(solution(p, c))
