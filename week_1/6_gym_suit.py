71efda2d8e1f24f203c72643e4ccba27ae4022e2
DongIkkk <w8h0412@gmail.com>
2022-11-09 18:32:22 +0900

    week_1 6

--- week_1/6_gym_suit.py
+++ week_1/6_gym_suit.py
@@ -1,3 +1,44 @@
 def solution(n, lost, reserve):
-    answer = 0
+    for i in lost:
+        if i in reserve:
+            lost.remove(i)
+            reserve.remove(i)
+
+    lost2 = lost.copy()
+    reserve2 = reserve.copy()
+
+    if n in reserve:
+        for i in reserve:
+            if (int(i) - 1) in lost:
+                lost.remove(int(i) - 1)
+            elif (int(i) + 1) in lost:
+                lost.remove(int(i) + 1)
+        answer = n - len(lost)
+
+    elif 1 in reserve:
+        for i in reserve:
+            if (int(i) + 1) in lost:
+                lost.remove(int(i) + 1)
+            elif (int(i) - 1) in lost:
+                lost.remove(int(i) - 1)
+        answer = n - len(lost)
+
+    else:
+        for i in reserve:
+            if (int(i) - 1) in lost:
+                lost.remove(int(i) - 1)
+            elif (int(i) + 1) in lost:
+                lost.remove(int(i) + 1)
+        result = n - len(lost)
+        for i in reserve2:
+            if (int(i) + 1) in lost2:
+                lost2.remove(int(i) + 1)
+            elif (int(i) - 1) in lost:
+                lost2.remove(int(i) - 1)
+        result2 = n - len(lost)
+
+        answer = max(result, result2)
+
+    answer = n - len(lost)
+
     return answer
